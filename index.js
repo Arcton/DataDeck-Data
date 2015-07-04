2dfb621099b647dd5b513fef9abc02256dcd74ec
Jeremy Symon <jtsymon@gmail.com>
2015-07-04 14:00:59 +1200

    Handle mkdir error when directory exists

--- index.js
+++ index.js
@@ -4,19 +4,23 @@ exports.generate = function(dir) {
     if(dir === undefined || dir === null) {
         dir = __dirname;
     }
-    fs.mkdir(dir);
-    var decks = ls(__dirname + "/decks/*.js");
-    for(var i = 0, len = decks.length; i < len; i++) {
-        var json = require(decks[i].full).generate();
-        var out = JSON.stringify(json, null, 2);
-        var filename = json.name.replace(" ", "_").replace("[^a-zA-Z0-9]", "");
-        fs.writeFile(dir + "/" + filename + ".json", out, function(err) {
-            if(err) {
-                return console.log(err);
-            }
-            console.log(filename + " was saved!");
-        });
-    }
+    fs.mkdir(dir, 0777, function(err) {
+        if(err && err.code != 'EEXIST') {
+            return console.error(err);
+        }
+        var decks = ls(__dirname + "/decks/*.js");
+        for(var i = 0, len = decks.length; i < len; i++) {
+            var json = require(decks[i].full).generate();
+            var out = JSON.stringify(json, null, 2);
+            var filename = json.name.replace(" ", "_").replace("[^a-zA-Z0-9]", "");
+            fs.writeFile(dir + "/" + filename + ".json", out, function(err) {
+                if(err) {
+                    return console.log(err);
+                }
+                console.log(filename + " was saved!");
+            });
+        }
+    });
 }
 if(module.parent == null) {
     exports.generate();
